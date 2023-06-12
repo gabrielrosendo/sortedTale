66897bdb992fd69f2b7cfb21376a7aaaf324076d
GABRIELROSENDO <GABRIELROSEND011@GMAIL.COM>
2023-06-11 20:13:44 -0400

    commit

--- app.py
+++ app.py
@@ -3,28 +3,31 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-bookshelf = utils.bookshelf
+library = utils.bookshelf
 
 collection = utils.collection
-print(bookshelf)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', books=bookshelf)
+    return render_template('index.html', books=library)
 
-@app.route('/found/', defaults={'bookname': ''}, methods=['GET', 'POST'])
-@app.route('/found/<path:bookname>', methods=['GET', 'POST'])
-def found(bookname):
-    if request.method == 'POST' or bookname:
-        search_term = str(bookname or request.form.get("bookname")).strip()
-        book = collection.find_one({"title": search_term.lower()})
-        if book:
-            return render_template('found.html', bookname=book)
+@app.route('/found/', defaults={'bookshelf': ''}, methods=['GET', 'POST'])
+@app.route('/found/<path:bookshelf>', methods=['GET', 'POST'])
+def found(bookshelf):
+    if request.method == 'POST' or bookshelf:
+        search_term = str(bookshelf or request.form.get("searchterm")).strip()
+        search_option = request.form.get("searchby")
+        book_s = collection.find({search_option: search_term})
+        if bookshelf:
+            return render_template('found.html', bookshelf=book_s)
         else:
             return render_template('found.html', error="Book not found")
     return render_template('found.html', error="Book not found")
 
+@app.route('/about', methods = ['GET', 'POST'])
+def about():
+  return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
