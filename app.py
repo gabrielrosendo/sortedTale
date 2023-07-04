5ddff3bb036c1b2eb3d7f2f231684463528301a1
GABRIELROSENDO <GABRIELROSEND011@GMAIL.COM>
2023-07-04 15:36:32 -0400

    Added quick browsing by genre

--- app.py
+++ app.py
@@ -40,5 +40,16 @@ def bookpage(book):
     book_details = collection.find_one({"title": book})
     return render_template('bookpage.html', book=book_details)
 
+
+@app.route('/books/<path:genre>', methods=['GET', 'POST'])
+def books_by_genre(genre):
+    bookshelf_results = collection.find({"genre": genre})
+
+    if bookshelf_results:
+        return render_template('found.html', bookshelf=bookshelf_results)
+    else:
+        return render_template('found.html', error="No books found in this genre")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
