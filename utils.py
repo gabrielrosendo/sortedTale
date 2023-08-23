e39cfdb5f8f9af5ae11c7b4b1d5560b847657f44
GABRIELROSENDO <gabrielrosendo72@gmail.com>
2023-08-23 19:05:24 -0300

    fixed mongo to get info from cloud server

--- utils.py
+++ utils.py
@@ -1,11 +1,13 @@
 import pymongo
+import certifi
+ca = certifi.where()
 
 # set up a conninsection to MongoDB
-client = pymongo.MongoClient("mongodb://localhost:27017/")
+client = pymongo.MongoClient("mongodb+srv://gabrielrosendo72:CvjdoUSPq49HOxsm@cluster0.tg1qb5x.mongodb.net/", tlsCAFile=ca)
 
 # specify the database and collection you want to insert data into
-db = client["mybookstore"]
-collection = db["book_information"]
+db = client["bookstore"]
+collection = db["books"]
 
 # create a dictionary with the data you want to insert
 results = collection.find()
